032b1befd6fd18143b0c638c65485510a1c36dae
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2022-10-14 16:01:13 +0100

    update

--- index.js
+++ index.js
@@ -53,8 +53,19 @@ const minAndMax = function () {
   const max = Math.max(...arrayRandom)
   console.log("The maximum value is " + max)
 }
-
 console.log(minAndMax())
+
+//Exercise 28
+
+let thisArr = []
+for (let i = 0; i < 4; i++) {
+  let current = []
+  for (let j = 0; j < 10; j++) {
+    current.push(Math.floor(Math.random() * 10))
+  }
+  thisArr.push(current)
+}
+console.log(thisArr)
 
 //Exercise 31
 
@@ -72,9 +83,11 @@ console.log(getAllTd)
 
 const printTd = function () {
   for (let i = 0; i < getAlltd.length; i++) {
-    console.log(getAlltd[i].innerText)
+    getAlltd[i].innerText
   }
 }
+console.log(printTd)
+
 //Exercise 34
 
 const changeH1 = function () {
@@ -90,4 +103,43 @@ const extraRow = function () {
   addERow.className = "tr"
   addERow.innerText = "This row was created with JS"
   cTable.appendChild(addERow)
+}
+console.log(extraRow)
+
+//Exercise 36
+
+const addClass = function () {
+  const tableRow = document.querySelectorAll("tr")
+  for (let i = 0; i < tableRow.length; i++) {
+    tableRow[i].classList.add("test")
+  }
+}
+
+//Exercise 37
+
+const addRedBackground = function () {
+  const links = document.querySelectorAll("a")
+  for (let i = 0; i < links.length; i++) {
+    links[i].style.backgroundColor = "red"
+  }
+}
+
+//Exercise 38
+
+window.onload = function () {}
+
+//Exercise 39
+const addNewListItems = function () {
+  const list = document.querySelector("ul")
+  const newItem = document.createElement("li")
+  newItem.innerText = "This list item was created using JS"
+  newItem.style.color = "red"
+  list.appendChild(newItem)
+}
+
+//Exercise 40
+const deleteList = function () {
+  const list = document.querySelector("ol")
+  console.log(list)
+  list.innerHtml = ""
 }
